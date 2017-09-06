599f904daa26d5d65f6fa068ec804f215c231418
François Cerbelle <francois@cerbelle.net>
2017-09-06 10:55:18 +0200

    Updated with latest Angular Viz from David (graph rate refresh limitation)

--- public/app.js
+++ public/app.js
@@ -1,10 +1,17 @@
 //-- author: david.maier@redislabs.com
 
+//Refresh rate
+const RATE = 1000;
+
 //Local cache
 var cache = {};
 
 //Set of all MAC addresses
 var macs = {};
+
+
+//Refresh counter
+var refresh = 0;
 
 //Helpers
 function _isDefined(obj)
@@ -45,81 +52,98 @@ var myApp = angular.module('iotDemo', []);
 myApp.controller('ValuesController', [ '$scope', '$window', function ValuesController($scope, $window) {
 	
 	var socket = $window.io.connect('http://localhost:3000');
+
 	socket.on('update', function (data) {
-    	
-		if ( !_isDefined(data["macaddr"] )) return;
-		//console.log(data);
 
-		var mac = data.macaddr;
+        if (data == []) return;
 
-		//Mimic a set
-		macs[mac] = mac;		
+		//Log client side
+        //console.log(data);
+
+        //Refresh counter
+        refresh = refresh+1;
+
+		//Value
+        var last = data.values[0];
+
+        //Mac address
+        var mac = data.macaddr;
+
+        //Mimic a set of MAC addresses
+        macs[mac] = mac;
+
+        //Init Cache
+        if (!_isDefined(cache[mac])) {
+            cache[mac] = _initArr(100);
+            cache['count::' + mac] = 0;
+            cache['last::' + mac] = "";
+
+            if (_isDefined(data.screenname)) {
+                cache['name::' + mac] = data.screenname;
+            }
+            else {
+                cache['name::' + mac] = mac;
+            }
+        }
+
+        //Set the last item anyway
+        cache['last::' + mac] = last;
 
 
-		//Init Cache
-		if ( !_isDefined(cache[mac] )) {
-			cache[mac] = _initArr(100);
-			cache['count::' + mac] = 0;
-			cache['last::' + mac] = "";
-			
-			if (_isDefined(data.screenname)) {
-			  cache['name::' + mac] = data.screenname;
-			}
-			else {
-			  cache['name::' + mac] = mac;
-			}
-		}
-		
-		//Cache the last 100 items
-		last = data.values[0];
-		var count = cache['count::' + mac];
-		
-		cache['last::' + mac] = last;
-		cache[mac][count] = last;
+        //Cache the last 100 values per MAC address but dependent on the  global refresh rate
+		if ( refresh - RATE == 0 ) {
 
-		count = count + 1;
-		//Round Robin
-		//if (count == cache[mac].length-1) count = 0;
-		
-		//Shifting
-		if (count == cache[mac].length-1) {
-		
-			cache[mac].shift();
-			cache[mac].push("");
-			count = count - 1;
-		}
+			refresh = 0;
 
-		cache['count::' + mac] = count;
+            var count = cache['count::' + mac];
+            cache[mac][count] = last;
+            count = count + 1;
+            //Round Robin
+            //if (count == cache[mac].length-1) count = 0;
 
-		//Set the model
-		$scope.items = cache;
-		$scope.macs = Object.values(macs);
-		$scope.$apply();
-		
+            //Shifting
 
-		//Data to draw
-		var chart_data = [];
-		chart_data.push(['Item', 'Value']);
-		var values = cache[mac];
+			if (count == cache[mac].length - 1) {
 
-		for (var i in values) {
-  			chart_data.push([ i , parseInt(values[i])]);
-		}			
-		
+                cache[mac].shift();
+                cache[mac].push("");
+                count = count - 1;
+            }
 
-		//Draw chart
-		var data = google.visualization.arrayToDataTable(chart_data);
-      		
-		var color = 'red'
 
-		var options = {
-			colors: [color],
-        		title: 'Last 100 items'
-      		};
-      		
-		var chart = new $window.google.visualization.LineChart(document.getElementById(mac));
-      		//TODO: Draw dependent on property 'hidden'
-        //chart.draw(data, options);
+            cache['count::' + mac] = count;
+
+
+            //Data to draw
+            var chart_data = [];
+            chart_data.push(['Item', 'Value']);
+            var values = cache[mac];
+
+            for (var i in values) {
+                chart_data.push([i, parseInt(values[i])]);
+            }
+
+
+            //Draw chart
+            var data = google.visualization.arrayToDataTable(chart_data);
+
+            var color = 'red'
+
+            var options = {
+                colors: [color],
+                title: 'Last 100 items'
+            };
+
+            var chart = new $window.google.visualization.LineChart(document.getElementById(mac));
+            //TODO: Draw dependent on property 'hidden'
+            chart.draw(data, options);
+
+        }
+
+        //Set the model
+        $scope.items = cache;
+        $scope.macs = Object.values(macs);
+        $scope.$apply();
   	});
 }]);
 
